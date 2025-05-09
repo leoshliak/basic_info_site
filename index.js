18f422fa4ae06481733234224bf6afe2c2e11cef
leoshliak <levshlyak@gmail.com>
2025-05-09 12:14:23 +0300

    Improved version

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-const pages = ["index", "about", "contact"];
+const routes = {
+  '/': 'index.html',
+  '/about': 'about.html',
+  '/contact-me': 'contact-me.html'
+};
 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  let filename
-  if (q.pathname == "/") {
-    filename = "./index.html";
-  }
-   else if (!pages.includes(q.pathname.substring(1))) {
-   filename = "./404.html";
-   }
-  else {
-    filename = "." + q.pathname + ".html";  
-  }
-  fs.readFile(filename, function(err, data) {
+http.createServer((req, res) => {
+  const pathname = url.parse(req.url, true).pathname;
+  const filename = routes[pathname] || '404.html';
+
+  fs.readFile(filename, (err, data) => {
     if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      return res.end('Internal Server Error');
+    }
+    res.writeHead(filename === '404.html' ? 404 : 200, { 'Content-Type': 'text/html' });
+    res.end(data);
   });
 }).listen(8080);
